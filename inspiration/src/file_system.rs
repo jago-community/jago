75afc6dc680cfb4bbd1643131cf618b7c6874f5c
Isaac Snow <isaacjsnow@gmail.com>
2021-05-14 09:34:43 -0700

    file sending

---
+++ inspiration/src/file_system.rs
@@ -0,0 +1,482 @@
+//! source git@github.com:seanmonstar/warp.git
+//!
+//! Copyright (c) 2018-2020 Sean McArthur
+//!
+//! Permission is hereby granted, free of charge, to any person obtaining a copy
+//! of this software and associated documentation files (the "Software"), to deal
+//! in the Software without restriction, including without limitation the rights
+//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//! copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in
+//! all copies or substantial portions of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//! THE SOFTWARE.
+//!
+//! File System Filters
+
+use std::cmp;
+use std::fs::Metadata;
+use std::future::Future;
+use std::io;
+use std::path::{Path, PathBuf};
+use std::pin::Pin;
+use std::sync::Arc;
+use std::task::Poll;
+
+use bytes::{Bytes, BytesMut};
+use futures::future::Either;
+use futures::{future, ready, stream, FutureExt, Stream, StreamExt, TryFutureExt};
+use headers::{
+    AcceptRanges, ContentLength, ContentRange, ContentType, HeaderMapExt, IfModifiedSince, IfRange,
+    IfUnmodifiedSince, LastModified, Range,
+};
+use http::StatusCode;
+use hyper::{Body, Response};
+use mime_guess;
+use percent_encoding::percent_decode_str;
+use tokio::fs::File as TkFile;
+use tokio::io::AsyncSeekExt;
+use tokio_util::io::poll_read_buf;
+
+#[derive(Debug)]
+enum Error {
+    BadPath,
+    Machine(std::io::Error),
+    Headers(hyper::header::ToStrError),
+    Range(http_range::HttpRangeParseError),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::BadPath => write!(f, "bad path"),
+            Error::Machine(error) => write!(f, "{}", error),
+            Error::Headers(error) => write!(f, "{}", error),
+            Error::Range(error) => write!(f, "{:?}", error),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::BadPath => None,
+            Error::Machine(error) => Some(error),
+            Error::Headers(error) => Some(error),
+            Error::Range(error) => None,
+        }
+    }
+}
+
+impl From<http_range::HttpRangeParseError> for Error {
+    fn from(error: http_range::HttpRangeParseError) -> Self {
+        Self::Range(error)
+    }
+}
+
+impl From<hyper::header::ToStrError> for Error {
+    fn from(error: hyper::header::ToStrError) -> Self {
+        Self::Headers(error)
+    }
+}
+
+fn sanitize_path(base: impl AsRef<Path>, tail: &str) -> Result<PathBuf, Error> {
+    let mut buf = PathBuf::from(base.as_ref());
+    let p = match percent_decode_str(tail).decode_utf8() {
+        Ok(p) => p,
+        Err(err) => {
+            tracing::debug!("dir: failed to decode route={:?}: {:?}", tail, err);
+            return Err(Error::BadPath);
+        }
+    };
+    tracing::trace!("dir? base={:?}, route={:?}", base.as_ref(), p);
+    for seg in p.split('/') {
+        if seg.starts_with("..") {
+            tracing::warn!("dir: rejecting segment starting with '..'");
+            return Err(Error::BadPath);
+        } else if seg.contains('\\') {
+            tracing::warn!("dir: rejecting segment containing with backslash (\\)");
+            return Err(Error::BadPath);
+        } else {
+            buf.push(seg);
+        }
+    }
+    Ok(buf)
+}
+
+#[derive(Debug)]
+struct Conditionals {
+    if_modified_since: Option<IfModifiedSince>,
+    if_unmodified_since: Option<IfUnmodifiedSince>,
+    if_range: Option<IfRange>,
+    range: Option<Range>,
+}
+
+enum Cond {
+    NoBody(hyper::Response<Body>),
+    WithBody(Option<Range>),
+}
+
+impl Conditionals {
+    fn check(self, last_modified: Option<LastModified>) -> Cond {
+        if let Some(since) = self.if_unmodified_since {
+            let precondition = last_modified
+                .map(|time| since.precondition_passes(time.into()))
+                .unwrap_or(false);
+
+            tracing::trace!(
+                "if-unmodified-since? {:?} vs {:?} = {}",
+                since,
+                last_modified,
+                precondition
+            );
+            if !precondition {
+                let mut res = hyper::Response::new(Body::empty());
+                *res.status_mut() = StatusCode::PRECONDITION_FAILED;
+                return Cond::NoBody(res);
+            }
+        }
+
+        if let Some(since) = self.if_modified_since {
+            tracing::trace!(
+                "if-modified-since? header = {:?}, file = {:?}",
+                since,
+                last_modified
+            );
+            let unmodified = last_modified
+                .map(|time| !since.is_modified(time.into()))
+                // no last_modified means its always modified
+                .unwrap_or(false);
+            if unmodified {
+                let mut res = Response::new(Body::empty());
+                *res.status_mut() = StatusCode::NOT_MODIFIED;
+                return Cond::NoBody(res);
+            }
+        }
+
+        if let Some(if_range) = self.if_range {
+            tracing::trace!("if-range? {:?} vs {:?}", if_range, last_modified);
+            let can_range = !if_range.is_modified(None, last_modified.as_ref());
+
+            if !can_range {
+                return Cond::WithBody(None);
+            }
+        }
+
+        Cond::WithBody(self.range)
+    }
+}
+
+fn conditionals(request: &hyper::Request<Body>, size: u64) -> Result<Conditionals, Error> {
+    let headers = request.headers();
+
+    Ok(Conditionals {
+        if_modified_since: headers.typed_get(),
+        if_unmodified_since: headers.typed_get(),
+        if_range: headers.typed_get(),
+        range: headers.typed_get(),
+    })
+}
+
+/// A file response.
+#[derive(Debug)]
+pub struct File {
+    resp: hyper::Response<Body>,
+    path: ArcPath,
+}
+
+impl File {
+    /// Extract the `&Path` of the file this `Response` delivers.
+    ///
+    /// # Example
+    ///
+    /// The example below changes the Content-Type response header for every file called `video.mp4`.
+    ///
+    /// ```ignore
+    /// use warp::{Filter, reply::Reply};
+    ///
+    /// let route = warp::path("static")
+    ///     .and(warp::fs::dir("/www/static"))
+    ///     .map(|reply: warp::filters::fs::File| {
+    ///         if reply.path().ends_with("video.mp4") {
+    ///             warp::reply::with_header(reply, "Content-Type", "video/mp4").into_response()
+    ///         } else {
+    ///             reply.into_response()
+    ///         }
+    ///     });
+    /// ```
+    pub fn path(&self) -> &Path {
+        self.path.as_ref()
+    }
+}
+
+// Silly wrapper since Arc<PathBuf> doesn't implement AsRef<Path> ;_;
+#[derive(Clone, Debug)]
+struct ArcPath(Arc<PathBuf>);
+
+impl AsRef<Path> for ArcPath {
+    fn as_ref(&self) -> &Path {
+        (*self.0).as_ref()
+    }
+}
+
+impl From<File> for hyper::Response<Body> {
+    fn from(file: File) -> Self {
+        file.resp
+    }
+}
+
+fn file_reply(
+    path: ArcPath,
+    conditionals: Conditionals,
+) -> impl Future<Output = Result<File, Error>> + Send {
+    TkFile::open(path.clone()).then(move |res| match res {
+        Ok(f) => Either::Left(file_conditional(f, path, conditionals)),
+        Err(err) => Either::Right(future::err(Error::Machine(err))),
+    })
+}
+
+async fn file_metadata(f: TkFile) -> Result<(TkFile, Metadata), Error> {
+    match f.metadata().await {
+        Ok(meta) => Ok((f, meta)),
+        Err(err) => {
+            tracing::debug!("file metadata error: {}", err);
+            Err(Error::Machine(err))
+        }
+    }
+}
+
+fn file_conditional(
+    f: TkFile,
+    path: ArcPath,
+    conditionals: Conditionals,
+) -> impl Future<Output = Result<File, Error>> + Send {
+    file_metadata(f).map_ok(move |(file, meta)| {
+        let mut len = meta.len();
+        let modified = meta.modified().ok().map(LastModified::from);
+
+        let resp = match conditionals.check(modified) {
+            Cond::NoBody(resp) => resp,
+            Cond::WithBody(range) => {
+                bytes_range(range, len)
+                    .map(|(start, end)| {
+                        let sub_len = end - start;
+                        let buf_size = optimal_buf_size(&meta);
+                        let stream = file_stream(file, buf_size, (start, end));
+                        let body = Body::wrap_stream(stream);
+
+                        let mut resp = Response::new(body);
+
+                        if sub_len != len {
+                            *resp.status_mut() = StatusCode::PARTIAL_CONTENT;
+                            resp.headers_mut().typed_insert(
+                                ContentRange::bytes(start..end, len).expect("valid ContentRange"),
+                            );
+
+                            len = sub_len;
+                        }
+
+                        let mime = mime_guess::from_path(path.as_ref()).first_or_octet_stream();
+
+                        resp.headers_mut().typed_insert(ContentLength(len));
+                        resp.headers_mut().typed_insert(ContentType::from(mime));
+                        resp.headers_mut().typed_insert(AcceptRanges::bytes());
+
+                        if let Some(last_modified) = modified {
+                            resp.headers_mut().typed_insert(last_modified);
+                        }
+
+                        resp
+                    })
+                    .unwrap_or_else(|BadRange| {
+                        // bad byte range
+                        let mut resp = Response::new(Body::empty());
+                        *resp.status_mut() = StatusCode::RANGE_NOT_SATISFIABLE;
+                        resp.headers_mut()
+                            .typed_insert(ContentRange::unsatisfied_bytes(len));
+                        resp
+                    })
+            }
+        };
+
+        File { resp, path }
+    })
+}
+
+struct BadRange;
+
+fn bytes_range(range: Option<Range>, max_len: u64) -> Result<(u64, u64), BadRange> {
+    use std::ops::Bound;
+
+    let range = if let Some(range) = range {
+        range
+    } else {
+        return Ok((0, max_len));
+    };
+
+    let ret = range
+        .iter()
+        .map(|(start, end)| {
+            let start = match start {
+                Bound::Unbounded => 0,
+                Bound::Included(s) => s,
+                Bound::Excluded(s) => s + 1,
+            };
+
+            let end = match end {
+                Bound::Unbounded => max_len,
+                Bound::Included(s) => {
+                    // For the special case where s == the file size
+                    if s == max_len {
+                        s
+                    } else {
+                        s + 1
+                    }
+                }
+                Bound::Excluded(s) => s,
+            };
+
+            if start < end && end <= max_len {
+                Ok((start, end))
+            } else {
+                tracing::trace!("unsatisfiable byte range: {}-{}/{}", start, end, max_len);
+                Err(BadRange)
+            }
+        })
+        .next()
+        .unwrap_or(Ok((0, max_len)));
+    ret
+}
+
+fn file_stream(
+    mut file: TkFile,
+    buf_size: usize,
+    (start, end): (u64, u64),
+) -> impl Stream<Item = Result<Bytes, io::Error>> + Send {
+    use std::io::SeekFrom;
+
+    let seek = async move {
+        if start != 0 {
+            file.seek(SeekFrom::Start(start)).await?;
+        }
+        Ok(file)
+    };
+
+    seek.into_stream()
+        .map(move |result| {
+            let mut buf = BytesMut::new();
+            let mut len = end - start;
+            let mut f = match result {
+                Ok(f) => f,
+                Err(f) => return Either::Left(stream::once(future::err(f))),
+            };
+
+            Either::Right(stream::poll_fn(move |cx| {
+                if len == 0 {
+                    return Poll::Ready(None);
+                }
+                reserve_at_least(&mut buf, buf_size);
+
+                let n = match ready!(poll_read_buf(Pin::new(&mut f), cx, &mut buf)) {
+                    Ok(n) => n as u64,
+                    Err(err) => {
+                        tracing::debug!("file read error: {}", err);
+                        return Poll::Ready(Some(Err(err)));
+                    }
+                };
+
+                if n == 0 {
+                    tracing::debug!("file read found EOF before expected length");
+                    return Poll::Ready(None);
+                }
+
+                let mut chunk = buf.split().freeze();
+                if n > len {
+                    chunk = chunk.split_to(len as usize);
+                    len = 0;
+                } else {
+                    len -= n;
+                }
+
+                Poll::Ready(Some(Ok(chunk)))
+            }))
+        })
+        .flatten()
+}
+
+fn reserve_at_least(buf: &mut BytesMut, cap: usize) {
+    if buf.capacity() - buf.len() < cap {
+        buf.reserve(cap);
+    }
+}
+
+const DEFAULT_READ_BUF_SIZE: usize = 8_192;
+
+fn optimal_buf_size(metadata: &Metadata) -> usize {
+    let block_size = get_block_size(metadata);
+
+    // If file length is smaller than block size, don't waste space
+    // reserving a bigger-than-needed buffer.
+    cmp::min(block_size as u64, metadata.len()) as usize
+}
+
+#[cfg(unix)]
+fn get_block_size(metadata: &Metadata) -> usize {
+    use std::os::unix::fs::MetadataExt;
+    //TODO: blksize() returns u64, should handle bad cast...
+    //(really, a block size bigger than 4gb?)
+
+    // Use device blocksize unless it's really small.
+    cmp::max(metadata.blksize() as usize, DEFAULT_READ_BUF_SIZE)
+}
+
+#[cfg(not(unix))]
+fn get_block_size(_metadata: &Metadata) -> usize {
+    DEFAULT_READ_BUF_SIZE
+}
+
+#[cfg(test)]
+mod tests {
+    use super::sanitize_path;
+    use bytes::BytesMut;
+
+    #[test]
+    fn test_sanitize_path() {
+        let base = "/var/www";
+
+        fn p(s: &str) -> &::std::path::Path {
+            s.as_ref()
+        }
+
+        assert_eq!(
+            sanitize_path(base, "/foo.html").unwrap(),
+            p("/var/www/foo.html")
+        );
+
+        // bad paths
+        sanitize_path(base, "/../foo.html").expect_err("dot dot");
+
+        sanitize_path(base, "/C:\\/foo.html").expect_err("C:\\");
+    }
+
+    #[test]
+    fn test_reserve_at_least() {
+        let mut buf = BytesMut::new();
+        let cap = 8_192;
+
+        assert_eq!(buf.len(), 0);
+        assert_eq!(buf.capacity(), 0);
+
+        super::reserve_at_least(&mut buf, cap);
+        assert_eq!(buf.len(), 0);
+        assert_eq!(buf.capacity(), cap);
+    }
+}
