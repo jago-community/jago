ecdd61eb1257303b34c3b4dc1c74f0cd0baf0bc2
Alec Thompson <isaacjsnow@gmail.com>
2021-10-18 10:46:52 -0700

    why things are the way they are

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,80 @@
-program::build!();
+fn main() {
+    let start = std::time::Instant::now();
+
+    let mut input = std::env::args().skip(1).peekable();
+    let mut code = 0;
+    let mut after: Vec<Box<dyn Fn()>> = vec![];
+
+    #[cfg(feature = "logs")]
+    if let Err(error) = pretty_env_logger::formatted_builder()
+        .filter_level(log::LevelFilter::Info)
+        .try_init()
+    {
+        eprintln!("{}", error);
+        code = weight(error);
+    }
+
+    #[cfg(feature = "logs")]
+    log::info!("starting execution");
+
+    let mut context = vec![];
+
+    let bounty = gather(&mut input, &mut context).map(move |_| context);
+
+    inspect(bounty);
+
+    #[cfg(feature = "logs")]
+    log::info!("{:?} elapsed", start.elapsed());
+
+    after.iter().for_each(|handle| handle());
+
+    std::process::exit(code as i32);
+}
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("Incomplete")]
+    Incomplete,
+}
+
+use std::{iter::Peekable, mem::replace};
+
+fn gather<'a>(
+    input: &mut Peekable<impl Iterator<Item = String>>,
+    context: &'a mut Vec<u8>,
+) -> Result<(), Error> {
+    let _difference = replace(context, b"why things are the way they are".to_vec());
+
+    Ok(())
+}
+
+fn inspect<Bounty: AsRef<[u8]>>(input: Result<Bounty, Error>) -> u32 {
+    let bounty = match input {
+        Ok(bounty) => bounty,
+        Err(error) => {
+            #[cfg(feature = "logs")]
+            log::error!("{}", error);
+
+            return weight(error);
+        }
+    };
+
+    report(bounty.as_ref());
+
+    0
+}
+
+fn weight<Input>(_input: Input) -> u32 {
+    1
+}
+
+fn report(input: &[u8]) {
+    #[cfg(feature = "logs")]
+    if let Ok(input) = std::str::from_utf8(input) {
+        log::info!("{}", input);
+    } else {
+        log::info!("{:?}", input);
+    }
+
+    drop(input);
+}
