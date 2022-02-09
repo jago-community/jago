4f1625343c74969eb64aa821c85de25821748199
Isaac Snow <isaacjsnow@gmail.com>
2022-02-09 10:49:21 -0800

    fresh

--- src/view.rs
+++ src/view.rs
@@ -1,13 +1,16 @@
-use ::{crossterm::Command, std::fmt};
+use ::{
+    crossterm::Command,
+    std::{fmt, io::Write},
+};
 
-pub trait View {
+pub trait View: Write {
     type Buffer;
 
-    fn view_ansi(self: &Self, out: &mut Self::Buffer) -> fmt::Result
+    fn view_ansi<C: Command>(self: &Self, out: ) -> fmt::Result
     where
         Self: Command,
         Self::Buffer: fmt::Write,
     {
-        self.write_ansi(out)
+        Ok(())
     }
 }
