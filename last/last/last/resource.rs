837252a7f367d04859fd5db76e49b595acd2d299
Isaac Snow <isaacjsnow@gmail.com>
2022-02-09 10:56:43 -0800

    fresh

--- last/last/last/resource.rs
+++ last/last/last/resource.rs
@@ -1,31 +1,44 @@
-use std::path::PathBuf;
-
-pub struct Resource {
-    path: PathBuf,
-    bytes: Option<Vec<u8>>,
-}
-
-#[derive(Debug, thiserror::Error)]
-enum Error {
-    #[error("Io")]
-    Io(#[from] std::io::Error),
-}
-
 use std::path::Path;
 
-impl From<&Path> for Resource {
-    fn from(path: &Path) -> Self {
-        Self {
-            path: path.into(),
-            bytes: None,
-        }
+pub struct Resource<'a>(&'a Path);
+
+impl<'a> From<&'a Path> for Resource<'a> {
+    fn from(path: &'a Path) -> Self {
+        Resource(path)
     }
 }
 
-// use std::{fs::OpenOptions, io::Read, path::Path};
+use std::fmt::Display;
 
-impl Resource {
-    pub fn bytes(&self) -> &[u8] {
-        unimplemented!()
+impl Display for Resource<'_> {
+    fn fmt(&self, out: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.display().fmt(out)
+    }
+}
+
+use crossterm::{style::Print, Command};
+
+impl Command for Resource<'_> {
+    fn write_ansi(&self, out: &mut impl std::fmt::Write) -> std::fmt::Result {
+        Print(self.0.display()).write_ansi(out)
+    }
+}
+
+use crate::handle::{Handle, Outcome};
+
+use crossterm::event::{Event, KeyCode, KeyEvent};
+
+impl Handle for Resource<'_> {
+    fn handle(&mut self, event: &Event) -> Outcome {
+        match event {
+            Event::Key(KeyEvent {
+                code: KeyCode::Enter,
+                ..
+            }) => {
+                // ...
+                Outcome::Done
+            }
+            _ => self.handle_common(event),
+        }
     }
 }
