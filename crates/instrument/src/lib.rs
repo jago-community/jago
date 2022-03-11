d9122d5d9554199aa1c99eafe4323421b0810544
Isaac Snow <isaacjsnow@gmail.com>
2022-03-11 16:12:03 -0600

    uh

--- crates/instrument/src/lib.rs
+++ crates/instrument/src/lib.rs
@@ -1,3 +1,11 @@
+pub mod prelude {
+    #[cfg(target_arch = "wasm32")]
+    pub use log::{debug, error, info, trace, warn};
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub use tracing::{debug, error, info, trace, warn};
+}
+
 #[cfg(target_arch = "wasm32")]
 pub fn before() {
     use log::Level;
@@ -23,12 +31,4 @@ pub fn before() {
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
-}
-
-pub mod prelude {
-    #[cfg(target_arch = "wasm32")]
-    pub use log::{debug, error, info, trace, warn};
-
-    #[cfg(not(target_arch = "wasm32"))]
-    pub use tracing::{debug, error, info, trace, warn};
 }
